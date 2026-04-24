737149764bb2e04fa3b8b53c630b3a88579f5ac0
LastHope48 <erenmehmetserdar87@gmail.com>
2026-04-24 19:50:56 +0300

    initial commit.

---
+++ main.py
@@ -0,0 +1,7 @@
+import os
+file_count=0
+for root,dirs,files in os.walk(os.path.expanduser("~")):
+    for file in files:
+        print(os.path.join(root,file))
+        file_count+=1
+print(f"\n {file_count} dosya gezildi!!!!!!!!!!!!")
